f2d9e9c23b274808ddfa15be8dcd4ae81db74c86
Adam  Ibrahim <a.h.ibrahim@student.tue.nl>
2023-05-15 15:14:31 +0200

    Retry from first

--- count.py
+++ count.py
@@ -1,9 +1,2 @@
-# TESTING
-# Second try
-for i in range(1, 11):
-    #Test
-    print(i)
-
-# there is no difference because I pushed the latest commit onto the main server anyways. 
-
-## Trial
+def function_name (void):
+    i = 0
